bab8ff87faac272cf033bd13775026b00a46a8ff
Ewelina Wiechowicz <ewelina.wiechowicz@gmail.com>
2018-11-25 17:13:12 +0100

    Updates

---
+++ precache-manifest.7a9a5f575a156d0556134823563e3729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c926c5c708dca142806",
+    "url": "/jmr/static/js/runtime~main.8c926c5c.js"
+  },
+  {
+    "revision": "794047bb42cf0759f845",
+    "url": "/jmr/static/js/main.794047bb.chunk.js"
+  },
+  {
+    "revision": "67d9075132c3528562fe",
+    "url": "/jmr/static/js/1.67d90751.chunk.js"
+  },
+  {
+    "revision": "794047bb42cf0759f845",
+    "url": "/jmr/static/css/main.e6c47fbc.chunk.css"
+  },
+  {
+    "revision": "8a510a0f53743a8e34382f21ae083890",
+    "url": "/jmr/index.html"
+  }
+];
